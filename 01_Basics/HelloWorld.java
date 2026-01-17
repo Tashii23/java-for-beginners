dc264ed4998f628af25c784700c63cb4ff260fb6
Tashii23 <aakhyamishrawork@gmail.com>
2026-01-17 21:50:01 +0530

    Add HelloWorld.java in 01_Basics folder

---
+++ 01_Basics/HelloWorld.java
@@ -0,0 +1,9 @@
+// Program: Hello World
+// Concept: Basics of Java
+// Level: Beginner (Class 9–10)
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
