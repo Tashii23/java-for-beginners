d66803fb687b5b43df545e92e16f9e1861c39b8d
Tashii23 <aakhyamishrawork@gmail.com>
2026-01-20 18:16:35 +0530

    Move grade_calculator.java in basics folder.

---
+++ 01_Basics/grade_calculator.java
@@ -0,0 +1,17 @@
+import java.util.*:
+public static void main()
+  {
+    Scanner sc=new Scanner(System.in);
+  System.out.println("INPUT YOUR MARKS");
+  int n=sc.nextInt(); 
+  if(n>=90)
+    System.out.println("GRADE A");
+  else if(n>=75)
+    System.out.println("GRADE B");
+  else if (n>=60)
+    System.out.println("GRADE C");
+  else if(n>=45)
+    System.out.println("GRADE D");
+  else if
+    System.out.println("FAIL");
+}
